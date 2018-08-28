fc22fbe24a7087da1eeaa5a3f275683bbf5941fd
Torrance Davenport <tjdavenport2@gmail.com>
2018-08-27 20:10:51 -0400

    geoscheme

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  countries: require('./countries'),
+  regions: require('./regions'),
+  subRegions: require('./subRegions'),
+  countryRegions: require('./countryRegions'),
+  countrySubRegions: require('./countrySubRegions'),
+};
---
+++ regions.js
@@ -0,0 +1,7 @@
+module.exports = {
+  1: { id: 1, name: 'Africa' },
+  2: { id: 2, name: 'Americas' },
+  3: { id: 3, name: 'Asia' },
+  4: { id: 4, name: 'Europe' },
+  5: { id: 5, name: 'Oceania' },
+};
